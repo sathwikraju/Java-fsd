b2cc616a38d0908f1b55aa28e24d8f4fa4e97f50
sathwikraju <128493507+sathwikraju@users.noreply.github.com>
2023-08-25 19:12:25 +0530

    Add files via upload

---
+++ Phase-4 Practice Project: Assisted Practice/Phase-4 Practice Project: Assisted Practice - 3/angular-TwoWayBinding/src/app/app.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: '<app-parent></app-parent>',
+})
+export class AppComponent {
+  title = 'angular-TwoWayBinding';
+}
